e5bd21f6e9968f8ce2ab38d8107edd4a65519d10
work <santhoshb.work@gmail.com>
2025-01-08 19:00:14 +0530

    Tested nta payment

--- Assets/3A_Catalog/Selfmade/Queries/Business partner with only one bpgroup mapped.sql
+++ Assets/3A_Catalog/Selfmade/Queries/Business partner with only one bpgroup mapped.sql
@@ -5,3 +5,28 @@ WHERE bpg.c_bpartner_id IN (
 GROUP BY
     bpg.c_bpartner_id ,cb.value
 HAVING COUNT(*) <=1;
+
+-----------------------------------------------
+
+Optimized
+
+SELECT  
+    bpg.c_bpartner_id, 
+    COUNT(*),
+    cb.value 
+FROM 
+    c_bpartner cb
+INNER JOIN 
+    um_bpartnergroup bpg 
+    ON bpg.c_bpartner_id = cb.c_bpartner_id 
+    AND cb.isactive = 'Y'
+INNER JOIN 
+    UM_BPartnerGroup bpg_filter
+    ON bpg.c_bpartner_id = bpg_filter.c_bpartner_id
+    AND bpg_filter.um_bpgrouptype = 'NTV'
+    AND bpg_filter.isactive = 'Y'
+GROUP BY
+    bpg.c_bpartner_id,
+    cb.value
+HAVING COUNT(*) <= 1;
+
